ccb1b008d73e70e7ac0bb65f517c52dc315a5371
josuenm <direct.josue@gmail.com>
2023-02-08 02:47:50 -0300

    feat: note entity added

--- src/config/mongodb/data-source.ts
+++ src/config/mongodb/data-source.ts
@@ -1,16 +1,17 @@
 import "dotenv/config";
 import { DataSource } from "typeorm";
+import { Note } from "../../modules/note/entities/Note";
 import { User } from "../../modules/user/entities/User";
 
 export const AppDataSource = new DataSource({
   type: "mongodb",
   host: "localhost",
-  synchronize: true,
+  synchronize: false,
   logging: true,
   authSource: "admin",
   username: process.env.MONGO_USER,
   password: process.env.MONGO_PASS,
-  entities: [User],
+  entities: [User, Note],
   subscribers: [],
   migrations: [],
 });
