2565cc617fefbc352dce976fe7b0634398a1e288
josuenm <direct.josue@gmail.com>
2023-02-14 00:27:57 -0300

    feat: using new color and text changed

--- src/services/nodemailer/templates/password-recovery-template.ts
+++ src/services/nodemailer/templates/password-recovery-template.ts
@@ -16,16 +16,16 @@ function passwordRecoveryTemplate(url: string, id: string) {
         style="width: 100%; max-width: 500px; padding: 0 10px; margin: 0 auto; text-align: center; background: #fff; color: #000; font-family: arial;">
         <div style="padding: 10px 0;">
           <img style="width:100px;" src="cid:logo" />
-          <h1 style="color: #26FFB1; text-align: center;">Purple Notes</h1>
+          <h1 style="color: #6b03fc; text-align: center;">Purple Notes</h1>
       </div>
               
         <h2>Resete sua senha</h2>
         <p style="color: #505050;">
-        Make a new password, click below to make a new password.
+        Create a new password, click below to make a new password.
         </p>
         <div style="padding: 20px 0;">
             <a href="${url}/password-recovery/confirm/${id}" target="_blank"
-              style="background-color: #26FFB1; color: inherit; text-decoration: none; font-weight: bold; border: 0; padding: 10px 20px; border-radius: 5px; cursor: pointer;">Create new password</a>
+              style="background-color: #6b03fc; color: inherit; text-decoration: none; font-weight: bold; border: 0; padding: 10px 20px; border-radius: 5px; cursor: pointer;">Create new password</a>
         </div>
       </div>
     `,
