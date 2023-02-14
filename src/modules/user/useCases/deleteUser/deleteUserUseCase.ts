4da917419cab90e11f7447caa6d40048c02bad67
josuenm <direct.josue@gmail.com>
2023-02-13 23:33:01 -0300

    feat: using note repository to delete all notes by the user

--- src/modules/user/useCases/deleteUser/deleteUserUseCase.ts
+++ src/modules/user/useCases/deleteUser/deleteUserUseCase.ts
@@ -1,9 +1,13 @@
 import createHttpError from "http-errors";
+import { INoteRepository } from "../../../note/repositories/implementations/INoteRepository";
 import { UserDocument } from "../../entities/User";
 import { IUserRepository } from "../../repositories/implementations/IUserRepository";
 
 class DeleteUserUseCase {
-  constructor(private userRepository: IUserRepository) {}
+  constructor(
+    private userRepository: IUserRepository,
+    private noteRepository: INoteRepository
+  ) {}
 
   public async execute(user: UserDocument, password: string) {
     if (!user.comparePassword(password)) {
@@ -16,6 +20,7 @@ class DeleteUserUseCase {
       throw createHttpError(401, "Incorrect password");
     }
 
+    await this.noteRepository.deleteAll(user._id);
     await this.userRepository.delete(user);
   }
 }
--- src/modules/user/useCases/deleteUser/index.ts
+++ src/modules/user/useCases/deleteUser/index.ts
@@ -1,9 +1,11 @@
+import { NoteRepository } from "../../../note/repositories/NoteRepository";
 import { UserRepository } from "../../repositories/UserRepository";
 import { DeleteUserController } from "./deleteUserController";
 import { DeleteUserUseCase } from "./deleteUserUseCase";
 
 const userRepository = new UserRepository();
-const deleteUserUseCase = new DeleteUserUseCase(userRepository);
+const noteRepository = new NoteRepository();
+const deleteUserUseCase = new DeleteUserUseCase(userRepository, noteRepository);
 const deleteUserController = new DeleteUserController(deleteUserUseCase);
 
 export { deleteUserController };
