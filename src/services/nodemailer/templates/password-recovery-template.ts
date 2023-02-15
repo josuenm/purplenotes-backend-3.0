272db55766a7cf7cc5ed42b8f6e8e2b2615a2d22
josuenm <direct.josue@gmail.com>
2023-02-15 13:56:34 -0300

    feat: text edited

--- src/services/nodemailer/templates/password-recovery-template.ts
+++ src/services/nodemailer/templates/password-recovery-template.ts
@@ -19,13 +19,13 @@ function passwordRecoveryTemplate(url: string, id: string) {
           <h1 style="color: #6b03fc; text-align: center;">Purple Notes</h1>
       </div>
               
-        <h2>Resete sua senha</h2>
+        <h2>Reset your password</h2>
         <p style="color: #505050;">
         Create a new password, click below to make a new password.
         </p>
         <div style="padding: 20px 0;">
             <a href="${url}/password-recovery/confirm/${id}" target="_blank"
-              style="background-color: #6b03fc; color: inherit; text-decoration: none; font-weight: bold; border: 0; padding: 10px 20px; border-radius: 5px; cursor: pointer;">Create new password</a>
+              style="background-color: #6b03fc; color: #fff; text-decoration: none; font-weight: bold; border: 0; padding: 10px 20px; border-radius: 5px; cursor: pointer;">Create new password</a>
         </div>
       </div>
     `,
