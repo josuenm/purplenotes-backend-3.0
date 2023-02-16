d23364ef6ae8afae010ff36498e57c1776d71f18
josuenm <direct.josue@gmail.com>
2023-02-15 21:16:05 -0300

    feat: route for account confirmation added

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import { auth } from "../middlewares/auth";
+import { confirmAccountController } from "../modules/user/useCases/confirmAccount";
 import { confirmPasswordRecoveryController } from "../modules/user/useCases/confirmPasswordRecovery";
 import { deleteUserController } from "../modules/user/useCases/deleteUser";
+import { getMyAccountConfirmationController } from "../modules/user/useCases/getMyAccountConfirmation";
 import { getPasswordRecoveryController } from "../modules/user/useCases/getPasswordRecovery";
+import { sendAccountConfirmationController } from "../modules/user/useCases/sendAccountConfirmation";
 import { sendPasswordRecoveryController } from "../modules/user/useCases/sendPasswordRecovery";
 import { signInController } from "../modules/user/useCases/signIn";
 import { signUpController } from "../modules/user/useCases/signUp";
@@ -11,6 +14,7 @@ import { updatePasswordController } from "../modules/user/useCases/updatePasswor
 
 const route = Router();
 
+// BEGIN: Access
 route.post("/sign-in", (req, res, next) => {
   signInController.handle(req, res, next);
 });
@@ -18,6 +22,7 @@ route.post("/sign-in", (req, res, next) => {
 route.post("/sign-up", (req, res, next) => {
   signUpController.handle(req, res, next);
 });
+// END: Access
 
 route.delete("/", auth, (req, res, next) => {
   deleteUserController.handle(req, res, next);
@@ -27,6 +32,7 @@ route.put("/basic-info", auth, (req, res, next) => {
   updateBasicInfoController.handle(req, res, next);
 });
 
+// BEGIN: Password
 route.put("/password", auth, (req, res, next) => {
   updatePasswordController.handle(req, res, next);
 });
@@ -42,5 +48,20 @@ route.post("/password-recovery/:id/confirm", (req, res, next) => {
 route.get("/password-recovery/:id", (req, res, next) => {
   getPasswordRecoveryController.handle(req, res, next);
 });
+// END: Password
+
+// BEGIN: Account confirmation
+route.get("/account-confirmation", auth, (req, res, next) => {
+  getMyAccountConfirmationController.handle(req, res, next);
+});
+
+route.post("/account-confirmation", auth, (req, res, next) => {
+  sendAccountConfirmationController.handle(req, res, next);
+});
+
+route.post("/account-confirmation/confirm", auth, (req, res, next) => {
+  confirmAccountController.handle(req, res, next);
+});
+// END: Account confirmation
 
 export { route as userRoute };
