950c1097a0c7862284ff3feaacf7b9c627519531
josuenm <direct.josue@gmail.com>
2023-02-17 22:58:15 -0300

    feat: url changed

--- src/config/mongodb/index.ts
+++ src/config/mongodb/index.ts
@@ -3,20 +3,19 @@ import mongoose from "mongoose";
 
 const MONGO_USER = process.env.MONGO_USER;
 const MONGO_PASS = process.env.MONGO_PASS;
-const MONGO_PORT = process.env.MONGO_PORT;
-const MONGO_HOST = process.env.MONGO_HOST;
 const MONGO_DB = process.env.MONGO_DB;
-
 let MONGO = "";
 
 if (process.env.NODE_ENV !== "production") {
-  MONGO = `${MONGO_HOST}:${MONGO_PORT}`;
+  const MONGO_PORT = process.env.MONGO_PORT;
+  const MONGO_HOST = process.env.MONGO_HOST;
+  MONGO = `mongodb://${MONGO_USER}:${MONGO_PASS}@${MONGO_HOST}:${MONGO_PORT}`;
 } else {
   MONGO = process.env.MONGO_URL || "";
 }
 
 mongoose.Promise = global.Promise;
-const MONGO_URL = `mongodb://${MONGO_USER}:${MONGO_PASS}@${MONGO}/${MONGO_DB}`;
+const MONGO_URL = `${MONGO}/${MONGO_DB}`;
 
 mongoose.set("strictQuery", true);
 
