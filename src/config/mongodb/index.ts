f3830825846bad8eeff1e6ff207b371706b4cbc7
Josué <direct.josue@gmail.com>
2023-04-07 19:54:09 -0300

    feat: using new url for production

--- src/config/mongodb/index.ts
+++ src/config/mongodb/index.ts
@@ -5,17 +5,18 @@ const MONGO_USER = process.env.MONGO_USER;
 const MONGO_PASS = process.env.MONGO_PASS;
 const MONGO_DB = process.env.MONGO_DB;
 const MONGO_HOST = process.env.MONGO_HOST;
+const MONGO_PARAMS = process.env.MONGO_PARAMS;
 let URL = "";
 
 if (process.env.NODE_ENV !== "production") {
   const MONGO_PORT = process.env.MONGO_PORT;
   URL = `mongodb://${MONGO_USER}:${MONGO_PASS}@${MONGO_HOST}:${MONGO_PORT}`;
 } else {
-  URL = `mongodb+srv://${MONGO_USER}:${MONGO_PASS}@${MONGO_HOST}`;
+  URL = `mongodb+srv://${MONGO_USER}:${MONGO_PASS}@${MONGO_HOST}/${MONGO_DB}${MONGO_PARAMS}`;
 }
 
 mongoose.Promise = global.Promise;
-const MONGO_URL = `${URL}/${MONGO_DB}`;
+const MONGO_URL = URL;
 
 mongoose.set("strictQuery", true);
 
